a47b9c30c47f69019f66ac95943072687986285a
craigrobertson <robertson.cra@gmail.com>
2024-10-28 19:10:24 +0000

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "logo-icons-web-app",
   plugins: [react()],
 });
